1db48a2c1531fa2d938994e7fc6e258c5ced15c3
Victor <101828330+iKanzo@users.noreply.github.com>
2026-03-08 19:22:00 +0500

    Остановка эффекта, если модалка закрыта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -34,7 +34,7 @@ export const ArticleParamsForm = ({
 	const menuRef = useRef<HTMLElement>(null);
 
 	const toggleForm = () => {
-		if (!isOpen) return;
+		if (!isMenuOpen) return;
 		setIsMenuOpen(!isMenuOpen);
 	};
 
